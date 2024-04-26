fbe704b8e1d6813b5d0cdd103c9ab420edb7a6b2
Soul <joshgingold336@gmail.com>
2024-04-26 14:01:08 -0500

    svg files, inquirer, jest, and inquirer questions added.

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require(`inquirer`);
+const Shape = require (`./lib/shapes`)
+
+inquirer.prompt([
+    {
+      type: `input`,
+      name: `characters`,
+      message: `What text should go instide of the svg? You can only use 3 characters.`,
+      validate: function (answer) {
+        if (answer.length > 3) {
+          return false;
+        }
+        return true;
+      },
+    },
+    {
+      type: `input`,
+      name: `textColor`,
+      message: `What color should the text be? Must be a valid color`,
+    },
+    {
+      type: `rawlist`,
+      name: `shape`,
+      message: `What shape would you like?`,
+      choices: [`circle`, `triangle`, `square`],
+    },
+    {
+      type: `input`,
+      name: `shapeColor`,
+      message: `What color should the svg be?`,
+    },
+  ]
+);
+.then((answers) => {
+console.log(answers);
+})
