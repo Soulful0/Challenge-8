1a74cd48a3ba5474db5456dab0a2cdbc530dc0fc
Soul <joshgingold336@gmail.com>
2024-04-27 20:59:07 -0500

    finished Jest in test.js, and fixed the squares proportions

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 const inquirer = require(`inquirer`);
-const Shape = require(`./lib/shapes`);
+const { Circle, Square, Triangle } = require(`./lib/shapes`);
 
-inquirer.prompt([
-  {
-    type: `input`,
-    name: `characters`,
-    message: `What text should go instide of the svg? You can only use 3 characters.`,
-    validate: function (answer) {
-      if (answer.length > 3) {
-        return false;
-      }
-      return true;
+inquirer
+  .prompt([
+    {
+      type: `input`,
+      name: `characters`,
+      message: `What text should go instide of the svg? You can only use 3 characters.`,
+      validate: function (answer) {
+        if (answer.length > 3) {
+          return false;
+        }
+        return true;
+      },
     },
-  },
-  {
-    type: `input`,
-    name: `textColor`,
-    message: `What color should the text be? Must be a valid color`,
-  },
-  {
-    type: `rawlist`,
-    name: `shape`,
-    message: `What shape would you like?`,
-    choices: [`circle`, `triangle`, `square`],
-  },
-  {
-    type: `input`,
-    name: `shapeColor`,
-    message: `What color should the svg be?`,
-  },
-]);
+    {
+      type: `input`,
+      name: `textColor`,
+      message: `What color should the text be? Must be a valid color`,
+    },
+    {
+      type: `rawlist`,
+      name: `shape`,
+      message: `What shape would you like?`,
+      choices: [`circle`, `triangle`, `square`],
+    },
+    {
+      type: `input`,
+      name: `shapeColor`,
+      message: `What color should the svg be?`,
+    },
+  ])
+  .then((answers) => {
+    let shape;
+    switch (answers.shape) {
+      case "Circle":
+        shape = new Circle();
+        break;
+      case "Triangle":
+        shape = new Triangle();
+        break;
+      case "Square":
+        shape = new Square();
+        break;
+    }
+
+    if (shape) {
+      shape.setColor(answers.shapeColor);
+      let svg = shape.render();
+      // Add text to the SVG
+      svg += `<text x="150" y="125" fill="${answers.textColor}" font-size="20" text-anchor="middle">${answers.characters}</text>`;
+      svg = `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400">${svg}</svg>`;
+      console.log(svg);
+    }
+  });
